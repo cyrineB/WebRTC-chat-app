cf4251784ca6026b755dc464f6e4b63f68b1d803
cyrine <cyrinebouaziz@outlook.com>
2020-11-16 20:13:44 +0100

    myApp

---
+++ signalrtc-backend-master/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using signalRtc.hubs;
+
+namespace signalRtc
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                    builder => builder.WithOrigins("http://localhost:4200", "https://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            services.AddSignalR();
+        }
+
+        readonly string MyAllowSpecificOrigins = "AllowOrigins";
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<SignalRtcHub>("/signalrtc");
+            });
+
+            app.Run(async(context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
---
+++ signalrtc-frontend-master/README.md
@@ -0,0 +1,7 @@
+# SignalRtc
+
+This project is the fronted part of the Angular and webRTC tutorial [LINK](https://dev.to/sebalr/video-call-with-webrtc-angular-and-asp-net-core-39hg)
+
+## Backend
+
+This is the .NET Core backend [LINK](https://github.com/sebalr/signalrtc-backend)
